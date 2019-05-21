5a954db7ccef0e68ea6b3db68269540daf2478b1
RegalWolf <iqbalhasan387@gmail.com>
2019-05-22 01:34:28 +0700

    register

--- validation/register.js
+++ validation/register.js
@@ -4,10 +4,13 @@ const isEmpty = require('./isEmpty');
 const registerValidation = data => {
   let errors = {};
 
+  errors.nama = null;
+  errors.username = null;
+  errors.password = null;
+
   data.nama = !isEmpty(data.nama) ? data.nama : '';
   data.username = !isEmpty(data.username) ? data.username : '';
   data.password = !isEmpty(data.password) ? data.password : '';
-  data.password2 = !isEmpty(data.password2) ? data.password2 : '';
 
   if (!Validator.isLength(data.nama, { min: 2, max: 50 })) {
     errors.nama = 'Nama harus 2 sampai 50 karakter';
@@ -31,14 +34,6 @@ const registerValidation = data => {
 
   if (Validator.isEmpty(data.password)) {
     errors.password = 'Password tidak boleh kosong';
-  }
-
-  if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = 'Password harus sama';
-  }
-
-  if (Validator.isEmpty(data.password2)) {
-    errors.password2 = 'Confirm Password tidak boleh kosong';
   }
 
   return {
